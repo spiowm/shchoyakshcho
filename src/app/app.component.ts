3089e991fc8f27b559bbe5a30471752b84c0f712
spiowm <oleksiitatar@gmail.com>
2024-12-12 14:29:57 +0200

    the first prototype

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Test1Component} from "./components/test1/test1.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Test1Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

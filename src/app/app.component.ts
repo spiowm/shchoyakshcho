977fa2ac0a1376dcf77fca52e1fac8eab4363c4b
spiowm <oleksiitatar@gmail.com>
2024-12-25 14:14:34 +0200

    make fico-calculator.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Test1Component} from "./components/test1/test1.component";
+import {FicoCalculatorComponent} from "./components/fico-calculator/fico-calculator.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Test1Component],
+  imports: [RouterOutlet, Test1Component, FicoCalculatorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

8df6ef92d2cee6a7a3e8acba8df261cfe9d2736b
spiowm <oleksiitatar@gmail.com>
2025-01-03 22:47:59 +0200

    add placeholder

--- src/app/components/fico-calculator/fico-calculator.component.ts
+++ src/app/components/fico-calculator/fico-calculator.component.ts
@@ -43,96 +43,112 @@ export class FicoCalculatorComponent {
       label: 'Загальний кредитний ліміт',
       description: 'Сукупний ліміт коштів, який ви можете позичити на всіх ваших рахунках та картках.',
       min: 0,
+      placeholder: 'USD'
     },
     {
       id: 'used_credit_amount',
       label: 'Використані кредитні кошти',
       description: 'Загальна сума, яку ви витратили з доступного кредитного ліміту.',
       min: 0,
+      placeholder: 'USD'
     },
     {
       id: 'available_credit_limit',
       label: 'Доступний кредит',
       description: 'Сума, яка ще доступна для використання в межах вашого кредитного ліміту.',
       min: 0,
+      placeholder: 'USD'
     },
     {
       id: 'accounts_with_late_payments',
       label: 'Рахунки з простроченнями',
       description: 'Кількість рахунків, на яких коли-небудь було протерміновано виплату.',
       min: 0,
+      placeholder: ''
     },
     {
       id: 'total_accounts',
       label: 'Загальна кількість рахунків',
       description: 'Загальна кількість ваших рахунків та кредитних карток.',
       min: 0,
+      placeholder: ''
     },
     {
       id: 'number_of_derogatory_records',
       label: 'Кількість негативних записів',
       description: 'Кількість негативних записів у вашій кредитній історії (наприклад, судові позови).',
       min: 0,
+      placeholder: ''
     },
     {
       id: 'number_of_collections',
       label: 'Кількість боргів у колекторах',
       description: 'Кількість боргів, переданих до колекторських агентств.',
       min: 0,
+      placeholder: ''
     },
     {
       id: 'months_since_first_credit',
       label: 'Місяців від першого кредиту',
       description: 'Кількість місяців, що минуло з моменту, коли ви відкрили перший кредитний рахунок або взяли перший кредит.',
       min: 0,
+      placeholder: ''
     },
     {
       id: 'accounts_with_75_percent_limit',
       label: 'Рахунки з більше ніж 75% доступного кредиту',
       description: 'Кількість рахунків, де залишок доступного кредиту перевищує 75%.',
       min: 0,
+      placeholder: ''
     },
     {
       id: 'credits_overdue_120_days',
       label: 'Прострочення понад 120 днів',
       description: 'Кількість кредитів, у яких прострочення перевищує 120 днів.',
       min: 0,
+      placeholder: ''
     },
     {
       id: 'total_taken_credits',
       label: 'Загальна кількість взятих кредитів',
       description: 'Загальна кількість кредитів або позик, які ви отримали.',
       min: 0,
+      placeholder: ''
     },
     {
       id: 'credits_taken_last_2_years',
       label: 'Кредити за останні 2 роки',
       description: 'Кількість кредитів, оформлених вами протягом останніх двох років.',
       min: 0,
+      placeholder: ''
     },
     {
       id: 'total_card_balance',
       label: 'Загальний баланс на картках',
       description: 'Загальна сума коштів, що знаходиться на всіх ваших картках.',
       min: 0,
+      placeholder: 'USD'
     },
     {
       id: 'total_income',
       label: 'Сумарний дохід',
       description: 'Ваш загальний дохід, який враховується при оцінці кредитоспроможності.',
       min: 0,
+      placeholder: 'USD'
     },
     {
       id: 'monthly_debt_payments',
       label: 'Щомісячні виплати боргу',
       description: 'Сума, яку ви сплачуєте щомісяця за своїми боргами.',
       min: 0,
+      placeholder: 'USD'
     },
     {
       id: 'credits_overdue_30_days',
       label: 'Прострочення понад 30 днів',
       description: 'Кількість кредитів, у яких прострочення перевищує 30 днів.',
       min: 0,
+      placeholder: ''
     }
   ];
 
